5f03c490559162f0958b337c95a7c2c751597f99
WEB Designer 2018.02 <38117932018.02@senacrjedu.local>
2018-11-07 12:09:18 -0200

    funcionalidade força de senha

--- cadastro.php
+++ cadastro.php
@@ -102,7 +102,11 @@ if (isset($_SESSION['valorcli'])) {
                                 <div class="col-md-4 mb-3">
                                     <label for="senha">Senha </label>
                                     <div class="input-group">                                        
-                                        <input type="password" class="form-control" id="senha" name="senha" placeholder="Digite a senha" value="">
+                                        <input type="password" class="form-control" id="senha" name="senha" placeholder="Digite a senha" value="">                                        
+                                    </div>
+                                    <div id="senhaBarra" class="progress" style="display: none;">
+                                        <div id="senhaForca" class="progress-bar bg-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="col-md-4 mb-3">
@@ -221,13 +225,12 @@ if (isset($_SESSION['valorcli'])) {
                             echo "<p class='text-danger'>campo numero aceita apenas digitos!</p>";
                         if (!$_SESSION['cond_cli']['complemento'])
                             echo "<p class='text-danger'>caracteres não aceitos no campo complemeto!</p>";
-                        if (isset($_SESSION['cond_cli']['cadastro_existente'])){
+                        if (isset($_SESSION['cond_cli']['cadastro_existente'])) {
                             echo "<p class='text-danger'>usuario já cadastrado!</p>";
                         }
                         if (!$_SESSION['cond_cli']['dpassword'])
                             echo "<p class='text-danger'>confirmação de senha invalida!</p>";
                         unset($_SESSION['cond_cli']);
-
                     }
                     ?>
                     <div class="form-group">
@@ -261,3 +264,93 @@ if (isset($_SESSION['valorcli'])) {
     <?php
     include 'rodape.php';
     ?> 
+
+    <script>
+
+
+        //Necessita do bootstrap e jquery
+        //forca da senha
+        $(function () {
+            $('#senha').keyup(function (e) {
+                var senha = $(this).val();
+                if (senha == '') {
+                    $('#senhaBarra').hide();
+                } else {
+                    var fSenha = forcaSenha(senha);
+                    var texto = "";
+                    $('#senhaForca').css('width', fSenha + '%');
+                    $('#senhaForca').removeClass();
+                    $('#senhaForca').addClass('progress-bar');
+                    if (fSenha <= 40) {
+                        texto = 'Fraca';
+                        $('#senhaForca').addClass('bg-danger');
+                    }
+
+                    if (fSenha > 40 && fSenha <= 70) {
+                        texto = 'Senha Média';
+                    }
+
+                    if (fSenha > 70 && fSenha <= 90) {
+                        texto = 'Senha Boa';
+                        $('#senhaForca').addClass('bg-info');
+                    }
+
+                    if (fSenha > 90) {
+                        texto = 'Senha Muito boa';
+                        $('#senhaForca').addClass('bg-success');
+                    }
+
+                    $('#senhaForca').text(texto);
+
+                    $('#senhaBarra').show();
+                }
+            });
+        });
+
+        function forcaSenha(senha) {
+            var forca = 0;
+            var regLetrasMa = /[A-Z]/;
+            var regLetrasMi = /[a-z]/;
+            var regNumero = /[0-9]/;
+            var regEspecial = /[!@#$%&*?]/;
+            var tam = false;
+            var tamM = false;
+            var letrasMa = false;
+            var letrasMi = false;
+            var numero = false;
+            var especial = false;
+//    console.clear();
+//    console.log('senha: '+senha);
+
+            if (senha.length >= 6)
+                tam = true;
+            if (senha.length >= 10)
+                tamM = true;
+            if (regLetrasMa.exec(senha))
+                letrasMa = true;
+            if (regLetrasMi.exec(senha))
+                letrasMi = true;
+            if (regNumero.exec(senha))
+                numero = true;
+            if (regEspecial.exec(senha))
+                especial = true;
+            if (tam)
+                forca += 10;
+            if (tamM)
+                forca += 10;
+            if (letrasMa)
+                forca += 10;
+            if (letrasMi)
+                forca += 10;
+            if (letrasMa && letrasMi)
+                forca += 20;
+            if (numero)
+                forca += 20;
+            if (especial)
+                forca += 20;
+//    console.log('força: '+forca);
+
+            return forca;
+        }
+
+    </script>
