a79f3644988b0f94d58aedb690e4b8716c2810d9
Rayane Cunha <raycusan@gmail.com>
2018-11-09 09:35:20 -0200

    Update pagadmin.php

--- pagadmin.php
+++ pagadmin.php
@@ -1,34 +1,34 @@
 <?php
-    include 'cabecalho.php'; 
-    if(isset($_SESSION['user'])){
-?>
-
-<div class="cover-container shadow-sm bg-white rounded d-flex h-50 p-5 ml-auto mr-auto mt-5 mb-5 flex-column">
-      
-
-      <div class="inner cover">
-        <h1 class="cover-heading mt-5">Bem-Vindo <?=ucfirst($_SESSION['user']['nome'])?></h1>
-        <p class="lead">Escolha uma das opções abaixo para ter acesso</p>
-        <p class="lead">
-          	<a href="index.php" class="btn btn-sm btn-warning">Página inicial</a><br>
-            <a href="atualizar.php" class="btn btn-sm btn-warning">Atualizar Dados</a><br>
-            <?php if($_SESSION['user']['permissao'] == "admin"){ ?>
-            <a href="cadastro_prod.php" class="btn btn-sm btn-warning">Cadastro de produto</a><br> <?php }?>
-			      
-            <a href="logout.php" class="btn btn-sm btn-warning">Log Out</a><br>
-		      	
-        </p>
+include 'cabecalho.php';
+if(isset($_SESSION['user'])){
+  ?>
+  <div class="cover-container bg-white text-center d-flex flex-column h-100 w-100 p-5 ml-auto mr-auto mt-1 mb-1" style="height: 25.8em">
+    <div class="inner cover mx-auto">
+      <hgroup class="mt-5 mb-3">
+        <h1 class="cover-heading">Bem-Vindo <?=ucfirst($_SESSION['user']['nome'])?></h1>
+        <h2 class="lead">Escolha uma das opções abaixo para ter acesso</h2>
+      </hgroup>
+      <div class="row">
+        <div class="btn-group mr-1">
+          <button type="button" onclick="location.href='index.php'" class="btn btn-secondary btn-block">Página Inicial</button>
+        </div>
+        <div class="btn-group mr-1">
+          <button type="button" onclick="location.href='atualizar.php'" class="btn btn-secondary btn-block">Atualizar Dados</button>
+        </div>
+        <?php if($_SESSION['user']['permissao'] == "admin"){ ?>
+          <div class="btn-group mr-1">
+            <button type="button" onclick="location.href='cadastro_prod.php'" class="btn btn-secondary btn-block">Cadastro de produto</button>
+          </div>
+        <?php }?>
+        <div class="btn-group mr-1">
+          <button type="button" onclick="location.href='logout.php'" class="btn btn-secondary btn-block">Log Out</button>
+        </div>
       </div>
-      
-
-
-</div>
-
-<?php include 'rodape.php';
+    </div>
+  </div>
+  <?php include 'rodape.php';
 }
 else{
   header("Location: login.php");
 }
 ?>
-
-
